b0cdbe2583a698d5c4ba95e03eae27cbe3b18b02
고수희 <dev.suhee@gmail.com>
2025-01-05 17:49:00 +0900

    feat: enhance command handling in main.ts to support 'type' command and improve user feedback for invalid commands

--- app/main.ts
+++ app/main.ts
@@ -5,13 +5,29 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+function getCommandType(command: string) {
+  switch (command) {
+    case 'echo':
+      return 'echo is a shell builtin';
+    case 'exit':
+      return 'exit is a shell builtin';
+    case 'type':
+      return 'type is a shell builtin';
+    default:
+      return `${command}: command not found`;
+  }
+}
+
 function runCommand() {
   rl.question('$ ', (answer: string) => {
-    if (answer === 'exit 0') {
-      rl.close();
+    if (answer.includes('type')) {
+      rl.write(`${getCommandType(answer.split('type ')[1])}\n`);
+      runCommand();
     } else if (answer.includes('echo')) {
       rl.write(`${answer.split('echo ')[1]}\n`);
       runCommand();
+    } else if (answer === 'exit 0') {
+      rl.close();
     } else {
       rl.write(`${answer}: command not found\n`);
       runCommand();
