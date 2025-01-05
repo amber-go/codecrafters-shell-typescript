15e67efd8c7c84e041903c04cc08416db6691c4d
고수희 <dev.suhee@gmail.com>
2025-01-05 17:14:29 +0900

    fix: streamline command handling in main.ts by always logging 'command not found'

--- app/main.ts
+++ app/main.ts
@@ -6,11 +6,5 @@ const rl = createInterface({
 });
 
 rl.question('$ ', (answer: string) => {
-  if (answer === 'invalid_command') {
-    console.log(`${answer}: command not found`);
-  } else {
-    console.log(answer);
-  }
-
-  rl.close();
+  console.log(`${answer}: command not found`);
 });
