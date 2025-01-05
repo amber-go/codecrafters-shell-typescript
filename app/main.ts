7067d7d084cff753f9c7f0daea899fcecac18aba
고수희 <dev.suhee@gmail.com>
2025-01-05 17:39:21 +0900

    fix: correct string splitting for echo command in main.ts to ensure proper output formatting

--- app/main.ts
+++ app/main.ts
@@ -10,7 +10,7 @@ function runCommand() {
     if (answer === 'exit 0') {
       rl.close();
     } else if (answer.includes('echo')) {
-      rl.write(`${answer.split('echo')[1]}\n`);
+      rl.write(`${answer.split('echo ')[1]}\n`);
       runCommand();
     } else {
       rl.write(`${answer}: command not found\n`);
