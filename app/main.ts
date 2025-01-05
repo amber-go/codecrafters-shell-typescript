705b9a69dda2fbfd0c620d29cf3128dfe63604b9
고수희 <dev.suhee@gmail.com>
2025-01-05 17:38:37 +0900

    feat: add support for echo command in main.ts to output text directly to the console

--- app/main.ts
+++ app/main.ts
@@ -9,6 +9,9 @@ function runCommand() {
   rl.question('$ ', (answer: string) => {
     if (answer === 'exit 0') {
       rl.close();
+    } else if (answer.includes('echo')) {
+      rl.write(`${answer.split('echo')[1]}\n`);
+      runCommand();
     } else {
       rl.write(`${answer}: command not found\n`);
       runCommand();
